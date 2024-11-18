83d256d6dadb581b8d5e801dc4f994641035d419
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2024-11-18 21:35:00 +0530

    python app documentation update

--- HealthCare_Python/form.py
+++ HealthCare_Python/form.py
@@ -7,25 +7,26 @@ UPLOAD_FOLDER = "uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
+#setting the routes
 @app.route("/", methods=["GET", "POST"])
 def index():
+#handling post request from frontend
     if request.method == "POST":
         name = request.form.get("name")
         age = request.form.get("age")
         file = request.files.get("file")
-
+#handling empty fields from server
         if not name or not age or not file:
             flash("All fields are required!")
             return redirect(url_for("index"))
-
+#saving the uploaded files in a local folder named uploads
         file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
         file.save(file_path)
-
+#printing the form inputs once it successfully collected from frontend
         print(f"Name: {name}")
         print(f"Age: {age}")
         print(f"Uploaded File: {file_path}")
-
+#sending message to the frontend
         flash("Form submitted successfully!")
         return redirect(url_for("index"))
 
